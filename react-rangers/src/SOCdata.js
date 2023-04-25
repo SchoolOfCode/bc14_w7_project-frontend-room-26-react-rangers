86624842e9bdfb9ceab9ad56dbce51c608818d06
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-04-25 16:12:04 +0100

    Information array created

---
+++ react-rangers/src/SOCdata.js
@@ -0,0 +1,25 @@
+let subject1 = {
+    title: "CSS",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    links: ["https://www.w3schools.com/css/", "https://www.w3schools.com/css/css_intro.asp"],
+    questionprompt: "",
+    question: "what is css?",
+    answer: "dunno",
+};
+let subject2 = {
+    title: "Functions",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    links: ["https://www.w3schools.com/css/", "https://www.w3schools.com/css/css_intro.asp"],
+    questionprompt: "",
+    question: "what is css?",
+    answer: "dunno",
+};
+let subject3 = {
+    title: "Variables",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    links: ["https://www.w3schools.com/css/", "https://www.w3schools.com/css/css_intro.asp"],
+    questionprompt: "",
+    question: "what is css?",
+    answer: "dunno",
+};
+export let SOCdata = [subject1, subject2, subject3]
