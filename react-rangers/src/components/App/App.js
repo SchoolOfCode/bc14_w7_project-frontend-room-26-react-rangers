b15e260405790105d3283388a7c89e855205d132
DeeqaJamalini <deeqa.j11@gmail.com>
2023-04-26 10:42:13 +0100

    Motivational quote rendering

--- react-rangers/src/components/App/App.js
+++ react-rangers/src/components/App/App.js
@@ -1,10 +1,33 @@
 
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  async function getMotivationalQuote() {
+    try {
+      const response = await fetch('https://type.fit/api/quotes');
+      const data = await response.json();
+      const randomQuote = data[Math.floor(Math.random() * data.length)];
+      setQuote(randomQuote);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMotivationalQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <p>hello</p>
+    <div>
+      {quote && <h1>Motivational Quote of the Day</h1>}
+      {quote ? (
+        <p>{quote.text} - {quote.author}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={getMotivationalQuote}>New Quote</button>
     </div>
   );
 }
